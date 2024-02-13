ddc415c775edd05c8a3c9f1fa534f96ea5638331
shanley2 <shanley907@gmail.com>
2024-02-13 10:25:39 -0500

    user authentication actions added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,24 +4,13 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-const doesExist = (username)=>{
-    let userswithsamename = users.filter((user)=>{
-      return user.username === username
-    });
-    if(userswithsamename.length > 0){
-      return true;
-    } else {
-      return false;
-    }
-}
-
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
 
     if (username && password) {
-        if (!doesExist(username)) {
-            users.push({"username":username,"passoword":password});
+        if (isValid(username)) {
+            users.push({"username":username,"password":password});
             return res.status(200).json({message: "User successfully registered. Now you can login"});
         } else {
             return res.status(404).json({message: "User already exists!"});
